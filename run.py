9256d1a0a00a12757fa8bd21e7d2678acc6060ca
kst <kst@users.noreply.github.com>
2026-05-29 08:50:59 +0900

    fix format

--- run.py
+++ run.py
@@ -9,4 +9,5 @@ os.environ["FOR_DISABLE_CONSOLE_CTRL_HANDLER"] = "1"
 if __name__ == "__main__":
     multiprocessing.freeze_support()
     import uvicorn
+
     uvicorn.run("app.main:app", host="0.0.0.0", port=8000, reload=True)
